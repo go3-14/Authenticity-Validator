90f3152022629bda4f56b1ac19fcfe70fc63d3a8
Avannish L <avannishl24bcs19@iiitkottayam.ac.in>
2025-09-23 14:50:40 +0530

    Before merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 const App=()=>{
+    const[data,setData] = useState(null);
+    useEffect(()=>{
+        fetch("http://127.0.0.1:5000/api/data")
+            .then((res)=>res.json())
+            .then((json)=>setData(json))
+            .catch((err)=>console.log("Error fetching data",err));
+    },[]);
+
     return(
-        <h1>App</h1>
+        <div>
+            <h1>Data from backend!</h1>
+            {data?(
+                <div>
+                    <p>Name:{data.name}</p>
+                    <p>Age:{data.age}</p>
+                    <p>Skills:{data.skills.join(', ')}</p>
+
+                </div>
+            ):(
+                <p>Loading...</p>
+            )}
+        </div>
+        
     );
 }
 
